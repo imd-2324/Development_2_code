97f4266d80e6d76bbcf100030896d452e9b61247
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2024-02-28 08:51:16 +0100

    Updates

--- 02_fileshare/server/server.js
+++ 02_fileshare/server/server.js
@@ -1,18 +1,18 @@
-// Stap 1: Importeer de benodigde modules
+// Stap 1: Importeer de nodige modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
 // Stap 2: Maak een nieuwe express applicatie
 const app = express();
+app.use(express.json()); // voor het parsen van application/json
+app.use(express.urlencoded({ extended: true })); // voor het parsen van application/x-www-form-urlencoded
 const port = 3000;
 
 // Stap 3: include client folder als static website
 const public_path = path.join(__dirname, '../client');
 app.use(express.static(public_path));
 
-app.use(express.json()); // voor het parsen van application/json
-app.use(express.urlencoded({ extended: true })); // voor het parsen van application/x-www-form-urlencoded
 
 // Stap 4: Bouw de API op
 
